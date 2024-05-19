97f6818240f6c598a5ce687dc62695549dddab4f
Mia012022 <wz1103202107@gmail.com>
2024-05-19 22:03:59 +0800

    回覆按鈕和整理

---
+++ DenGame/wwwroot/js/artical.js
@@ -0,0 +1,6 @@
+﻿$('.reply').on('click', function () {
+	
+	$(this).closest('.userComment').append('<div class="input-group mb-4 replyInput"><input type = "text" class = "form-control" placeholder = "快來發表你的評論吧" ><button class="btn btn-outline-secondary"><i class="bi bi-emoji-smile  "></i></button></i ><button class="btn btn-outline-secondary">發送</button></div > ');
+	$(this).attr('disabled', true);
+	
+})
